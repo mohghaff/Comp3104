bf7709b3076ee2752546017719e20c53c820bb78
DESKTOP-N8TRJ2G\mghaf <m.ghaffari.k@gmail.com>
2022-10-23 19:13:19 -0400

    React project code added

---
+++ build/precache-manifest.a0704bebf0c38f5275b2b3a99496b49b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5828d7c1c45374614389",
+    "url": "/mohghaff/Comp3104/static/css/main.c0018cef.chunk.css"
+  },
+  {
+    "revision": "5828d7c1c45374614389",
+    "url": "/mohghaff/Comp3104/static/js/main.5828d7c1.chunk.js"
+  },
+  {
+    "revision": "0d3c49d2fe5a3860fa7d",
+    "url": "/mohghaff/Comp3104/static/js/runtime~main.0d3c49d2.js"
+  },
+  {
+    "revision": "b1773f8afd9fb6a0106e",
+    "url": "/mohghaff/Comp3104/static/js/2.b1773f8a.chunk.js"
+  },
+  {
+    "revision": "8cff65539df076c4a369e9bc62ec90d2",
+    "url": "/mohghaff/Comp3104/index.html"
+  }
+];
